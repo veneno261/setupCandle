981e04761334c606b9c7b4ff6eff4b0ddaf87ba8
veneno261 <amiir.moghadam@yahoo.com>
2024-11-27 13:07:36 +0330

    edit 6

--- bin/candle_setup_finder.dart
+++ bin/candle_setup_finder.dart
@@ -16,7 +16,7 @@ void main(List<String> arguments) async {
 
   bot.sendMessage("bot start successfully");
 
-  // TODO: calculate sma degree and count it is
+  // TODO: calculate sma degree and count it isaaa
   // TODO: get chart screenshot
 
   /// list of calculate sma`s
