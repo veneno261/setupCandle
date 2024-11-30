dc99855c88060954e8c80593bd1466d39ef3995b
veneno261 <amiir.moghadam@gmail.com>
2024-11-30 08:34:58 +0330

    update fetch_chart_image.dart add sma 7-25-99 to chart

--- lib/apis/fetch_chart_image.dart
+++ lib/apis/fetch_chart_image.dart
@@ -26,6 +26,40 @@ class ChartImgApi {
       'height': height.toString(),
       'style': style,
       'theme': theme,
+      'studies':  [
+    {
+      "name": "Moving Average Multiple",
+      "input": {
+        "firstPeriods": 7,
+        "secondPeriods": 25,
+        "thirdPeriods": 99,
+       // "fourthPeriods": 50,
+       // "fifthPeriods": 100,
+       // "sixthPeriods": 200,
+        "method": "Simple"
+      },
+      "override": {
+        "Plot 1.linewidth": 1,
+        "Plot 1.plottype": "line",
+        "Plot 1.color": "rgb(156,39,176)",
+        "Plot 2.linewidth": 1,
+        "Plot 2.plottype": "line",
+        "Plot 2.color": "rgb(255,109,0)",
+        "Plot 3.linewidth": 1,
+        "Plot 3.plottype": "line",
+        "Plot 3.color": "rgb(67,160,71)",
+       // "Plot 4.linewidth": 1,
+        //"Plot 4.plottype": "line",
+        //"Plot 4.color": "rgb(38,198,218)",
+       // "Plot 5.linewidth": 1,
+       // "Plot 5.plottype": "line",
+       // "Plot 5.color": "rgb(245,0,87)",
+       // "Plot 6.linewidth": 1,
+       // "Plot 6.plottype": "line",
+       // "Plot 6.color": "rgb(33,150,243)"
+      }
+    }
+  ]
     };
 
     try {
